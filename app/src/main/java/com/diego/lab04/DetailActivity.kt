0d0ebff2eb18aea7b26f36bb364caae1be2985d4
Diego33KX <diegofj203@gmail.com>
2022-04-29 13:52:12 -0500

    Usando ViewBinding

--- app/src/main/java/com/diego/lab04/DetailActivity.kt
+++ app/src/main/java/com/diego/lab04/DetailActivity.kt
@@ -3,6 +3,8 @@ package com.diego.lab04
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.diego.lab04.databinding.ActivityDetailBinding
+import com.diego.lab04.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_detail.*
 
 const val PARAMETER_EXTRA_NOMBRE = "nombre"
@@ -10,38 +12,42 @@ const val PARAMETER_EXTRA_CORREO = "correo"
 const val PARAMETER_EXTRA_OFICINA = "oficina"
 const val PARAMETER_EXTRA_TELEFONO= "telefono"
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        //setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val extras = this.intent.extras
         if(extras != null){
             if(extras.get(PARAMETER_EXTRA_NOMBRE)!=null){
-                edtNombre.setText(extras.getString(PARAMETER_EXTRA_NOMBRE))
+                binding.edtNombre.setText(extras.getString(PARAMETER_EXTRA_NOMBRE))
             }
             if(extras.get(PARAMETER_EXTRA_CORREO)!=null){
-                edtCorreo.setText(extras.getString(PARAMETER_EXTRA_CORREO))
+                binding.edtCorreo.setText(extras.getString(PARAMETER_EXTRA_CORREO))
 
             }
             if(extras.get(PARAMETER_EXTRA_OFICINA)!=null){
-                edtOficina.setText(extras.getString(PARAMETER_EXTRA_OFICINA))
+                binding.edtOficina.setText(extras.getString(PARAMETER_EXTRA_OFICINA))
             }
             if(extras.get(PARAMETER_EXTRA_TELEFONO)!=null){
-                edtTelefono.setText(extras.getString(PARAMETER_EXTRA_TELEFONO))
+                binding.edtTelefono.setText(extras.getString(PARAMETER_EXTRA_TELEFONO))
             }
         }
     }
     fun CloseActivity(view: android.view.View){
-        val nombre = edtNombre.text.toString()
-        val correo = edtCorreo.text.toString()
-        val oficina = edtOficina.text.toString()
-        val telefono = edtTelefono.text.toString()
+        val nombre = binding.edtNombre.text.toString()
+        val correo = binding.edtCorreo.text.toString()
+        val oficina = binding.edtOficina.text.toString()
+        val telefono = binding.edtTelefono.text.toString()
 
         val intent = Intent(this,MainActivity::class.java)
-        intent.putExtra("nombre",nombre)
-        intent.putExtra("correo",correo)
-        intent.putExtra("oficina",oficina)
-        intent.putExtra("telefono",telefono)
+        intent.putExtra(PARAMETER_EXTRA_NOMBRE,nombre)
+        intent.putExtra(PARAMETER_EXTRA_CORREO,correo)
+        intent.putExtra(PARAMETER_EXTRA_OFICINA,oficina)
+        intent.putExtra(PARAMETER_EXTRA_TELEFONO,telefono)
         setResult(RESULT_OK,intent)
         finish()
     }
--- app/src/main/java/com/diego/lab04/MainActivity.kt
+++ app/src/main/java/com/diego/lab04/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.diego.lab04.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -17,26 +18,31 @@ const val PARAMETER_EXTRA_CORREO_NEW = "correo"
 const val PARAMETER_EXTRA_OFICINA_NEW = "oficina"
 const val PARAMETER_EXTRA_TELEFONO_NEW= "telefono"
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
     }
 
     fun sendExplicit(view: android.view.View){
-        val nombre = tvNombre.text.toString()
-        val correo = tvCorreo.text.toString()
-        val oficina = tvOficina.text.toString()
-        val telefono = tvTelefono.text.toString()
+
+        //ACCEDEMOS A LOS COMPONENTES CON VIEWBINDING
+        val nombre = binding.tvNombre.text.toString()
+        val correo = binding.tvCorreo.text.toString()
+        val oficina = binding.tvOficina.text.toString()
+        val telefono = binding.tvTelefono.text.toString()
         validateInputFields(nombre, correo, oficina,telefono)
         DetailActivity(nombre,correo,oficina,telefono)
     }
     private fun DetailActivity(nombre: String, correo: String, oficina: String, telefono: String){
         val intent = Intent(this, DetailActivity::class.java)
-        intent.putExtra("nombre",nombre)
-        intent.putExtra("correo",correo)
-        intent.putExtra("oficina",oficina)
-        intent.putExtra("telefono",telefono)
+        intent.putExtra(PARAMETER_EXTRA_NOMBRE,nombre)
+        intent.putExtra(PARAMETER_EXTRA_CORREO,correo)
+        intent.putExtra(PARAMETER_EXTRA_OFICINA,oficina)
+        intent.putExtra(PARAMETER_EXTRA_TELEFONO,telefono)
         startActivityForResult(intent,ACTIVITY_B_REQUEST)
 
     }
@@ -44,7 +50,6 @@ class MainActivity : AppCompatActivity() {
         if(nombre.isBlank() && correo.isBlank() && oficina.isBlank() && telefono.isBlank()) return
 
     }
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -61,35 +66,36 @@ class MainActivity : AppCompatActivity() {
                 val extras = data?.extras
                 if(extras != null){
                     if(extras.get(PARAMETER_EXTRA_NOMBRE_NEW)!=null){
-                        tvNombre.setText(extras.getString(PARAMETER_EXTRA_NOMBRE_NEW))
+                        binding.tvNombre.setText(extras.getString(PARAMETER_EXTRA_NOMBRE_NEW))
                     }
                     if(extras.get(PARAMETER_EXTRA_CORREO_NEW)!=null){
-                        tvCorreo.setText(extras.getString(PARAMETER_EXTRA_CORREO_NEW))
+                        binding.tvCorreo.setText(extras.getString(PARAMETER_EXTRA_CORREO_NEW))
                     }
                     if(extras.get(PARAMETER_EXTRA_OFICINA_NEW)!=null){
-                        tvOficina.setText(extras.getString(PARAMETER_EXTRA_OFICINA_NEW))
+                        binding.tvOficina.setText(extras.getString(PARAMETER_EXTRA_OFICINA_NEW))
                     }
                     if(extras.get(PARAMETER_EXTRA_TELEFONO_NEW)!=null){
-                        tvTelefono.setText(extras.getString(PARAMETER_EXTRA_TELEFONO_NEW))
+                        binding.tvTelefono.setText(extras.getString(PARAMETER_EXTRA_TELEFONO_NEW))
                     }
                 }
             }
         }
     }
 
-    fun make_calls(view: android.view.View){
-        val telefono = tvTelefono.text.toString()
+    fun makeCalls(view: android.view.View){
+        //val telefono = tvTelefono.text.toString()
+        val telefono = binding.tvTelefono.text.toString()
         val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", telefono, null))
         startActivity(intent);
     }
 
     fun whatsapp(view: android.view.View){
         try {
-            val correo = tvCorreo.text.toString()
-            val oficina = tvOficina.text.toString()
-            val nombre = tvNombre.text.toString()
+            val correo = binding.tvCorreo.text.toString()
+            val oficina = binding.tvOficina.text.toString()
+            val nombre = binding.tvNombre.text.toString()
+            val telefono = binding.tvTelefono.text.toString()
             val text = "Mensaje WSP para: Nombre: $nombre Correo:$correo Oficina:$oficina"
-            val telefono = tvTelefono.text.toString()
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse("http://api.whatsapp.com/send?phone=$telefono&text=$text")
             startActivity(intent)
@@ -97,10 +103,9 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
-    fun SendMessage(view: android.view.View){
-        val nombre = tvNombre.text.toString()
-        val telefono = tvTelefono.text.toString()
-        //startActivity(Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", telefono, null)))
+    fun sendMessage(view: android.view.View){
+        val nombre = binding.tvNombre.text.toString()
+        val telefono = binding.tvTelefono.text.toString()
         val uri = Uri.parse("smsto: $telefono")
         val it = Intent(Intent.ACTION_SENDTO, uri)
         it.putExtra("sms_body", "Hola $nombre te mando este mensaje desde Android")
